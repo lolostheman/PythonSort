0ca4f4b25e12c4bd4704fd1f0d91453caa873524
Logan Morro <loganmlogan@gmail.com>
2021-08-11 21:57:44 -0400

    bubble, merge, selection templates added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 arr = [1, 2, 3, 4]
@@ -6,12 +6,27 @@ arr = [1, 2, 3, 4]
 
 @app.route('/home')
 def test():
-    return render_template("index.html", content="pussy juice")
+    return render_template("index.html")
 
 
-@app.route("/<name>")
-def user(name):
-    return "Hello, " + "<h1>"+name+"</h1>"
+@app.route('/bubble_sort')
+def bubble():
+    return render_template("bubble.html")
+
+
+@app.route('/merge_sort')
+def merge():
+    return render_template("merge.html")
+
+
+@app.route("/selection_sort")
+def selection():
+    return render_template("selection.html")
+
+
+@app.route("/<usr>")
+def user(usr):
+    return "<h1>" + usr + "</h1>"
 
 
 @app.route("/admin")
